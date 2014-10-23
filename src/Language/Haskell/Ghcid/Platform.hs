09c3b48219032118dc5f3fad3de8cb9d8d013b39
Neil Mitchell <ndmitchell@gmail.com>
2014-10-23 18:04:54 +0100

    Move and generalise terminalTopmost to the Platform module

--- src/Language/Haskell/Ghcid/Platform.hs
+++ src/Language/Haskell/Ghcid/Platform.hs
@@ -1,12 +1,23 @@
 {-# LANGUAGE CPP #-}
 module Language.Haskell.Ghcid.Platform(
-    terminalSize
+    terminalSize, terminalTopmost,
     ) where
 
 #if !defined(mingw32_HOST_OS)
 import qualified System.Console.Terminal.Size as Terminal
 import System.IO (stdout)
 #endif
+
+#if defined(mingw32_HOST_OS)
+foreign import stdcall unsafe "windows.h GetConsoleWindow"
+    c_GetConsoleWindow :: IO Int
+
+foreign import stdcall unsafe "windows.h SetWindowPos"
+    c_SetWindowPos :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO Int
+
+c_HWND_TOPMOST = -1
+#endif
+
 
 -- | Figure out the size of the current terminal, width\/height, or return 'Nothing'.
 terminalSize :: IO (Maybe (Int, Int))
@@ -19,3 +30,11 @@ terminalSize = do
         Just w -> return $ Just (Terminal.width w, Terminal.height w)
         Nothing -> return Nothing
 #endif
+
+
+-- | Raise the current terminal on top of all other screens, if you can.
+terminalTopmost :: IO ()
+terminalTopmost = do
+    wnd <- c_GetConsoleWindow
+    c_SetWindowPos wnd c_HWND_TOPMOST 0 0 0 0 3
+    return ()
--- src/Main.hs
+++ src/Main.hs
@@ -5,27 +5,12 @@ module Main(main) where
 import System.Console.CmdArgs
 import Control.Monad
 import Language.Haskell.GhcidProgram
+import Language.Haskell.Ghcid.Platform
 import Language.Haskell.Ghcid.Util
 
-
-
-#if defined(mingw32_HOST_OS)
-foreign import stdcall unsafe "windows.h GetConsoleWindow"
-    c_GetConsoleWindow :: IO Int
-
-foreign import stdcall unsafe "windows.h SetWindowPos"
-    c_SetWindowPos :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> IO Int
-
-c_HWND_TOPMOST = -1
-#endif
 
 main :: IO ()
 main = do
     Options{..} <- cmdArgsRun options
-#if defined(mingw32_HOST_OS)
-    when topmost $ do
-        wnd <- c_GetConsoleWindow
-        c_SetWindowPos wnd c_HWND_TOPMOST 0 0 0 0 3
-        return ()
-#endif
+    when topmost terminalTopmost
     runGhcid command height (outStr . unlines)
