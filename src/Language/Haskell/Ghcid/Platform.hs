87a1cc7770f19f9778bea179c3377a6b16283597
Neil Mitchell <ndmitchell@gmail.com>
2014-10-23 18:02:13 +0100

    Docs and whitespace

--- src/Language/Haskell/Ghcid/Platform.hs
+++ src/Language/Haskell/Ghcid/Platform.hs
@@ -8,18 +8,14 @@ import qualified System.Console.Terminal.Size as Terminal
 import System.IO (stdout)
 #endif
 
+-- | Figure out the size of the current terminal, width\/height, or return 'Nothing'.
 terminalSize :: IO (Maybe (Int, Int))
-
 #if defined(mingw32_HOST_OS)
-
 terminalSize = return Nothing
-
 #else
-
 terminalSize = do
     s <- Terminal.hSize stdout
     return $ case s of
         Just w -> return $ Just (Terminal.width w, Terminal.height w)
         Nothing -> return Nothing
-
 #endif
