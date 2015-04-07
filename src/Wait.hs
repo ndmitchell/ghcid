8ee40ff90cf66e122171b9f274a971f9970e4874
Neil Mitchell <ndmitchell@gmail.com>
2015-04-07 16:07:57 +0100

    Avoid harmful warning bingo

--- src/Wait.hs
+++ src/Wait.hs
@@ -23,9 +23,9 @@ waitFiles = do
             [] -> recheck files new
             xs -> return xs
     where
-        recheck files' old = do
+        recheck files old = do
             sleep 0.1
-            new <- mapM getModTime files'
-            case [x | (x,t1,t2) <- zip3 files' old new, t1 /= t2] of
-                [] -> recheck files' new
+            new <- mapM getModTime files
+            case [x | (x,t1,t2) <- zip3 files old new, t1 /= t2] of
+                [] -> recheck files new
                 xs -> return xs
