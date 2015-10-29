b1d2a75d6eb3949d6911f0701474598c2d642dd8
Neil Mitchell <ndmitchell@gmail.com>
2015-10-29 07:48:52 +0000

    Rewrite the high level test to be more data driven

--- src/Test/HighLevel.hs
+++ src/Test/HighLevel.hs
@@ -2,6 +2,7 @@
 module Test.HighLevel(highLevelTests) where
 
 import System.Directory
+import System.IO.Extra
 import System.FilePath
 import System.Environment
 import Control.Exception
@@ -20,10 +21,8 @@ highLevelTests = testGroup "High Level tests"
     ]
 
 testStartRepl :: TestTree
-testStartRepl = testCase "Start cabal repl" $ do
-    root <- createTestProject
-    print =<< getEnv "PATH"
-    withCabal $ \flags -> do
+testStartRepl = testCase "Start cabal repl" $
+    withTestProject $ \root -> withCabal $ \flags -> do
         (ghci,load) <- startGhci (unwords $ "cabal repl":flags) (Just root) True
         stopGhci ghci
         load @?=  [ Loading "B.C" (normalise "src/B/C.hs")
@@ -31,9 +30,8 @@ testStartRepl = testCase "Start cabal repl" $ do
                   ]
 
 testShowModules :: TestTree
-testShowModules = testCase "Show Modules" $ do
-    root <- createTestProject
-    withCabal $ \flags -> do
+testShowModules = testCase "Show Modules" $
+    withTestProject $ \root -> withCabal $ \flags -> do
         (ghci,_) <- startGhci (unwords $ "cabal repl":flags) (Just root) True
         mods <- showModules ghci
         stopGhci ghci
@@ -50,71 +48,63 @@ withCabal act = do
                      `finally` setEnv "GHC_PACKAGE_PATH" path
 
 
-testProjectName = "BWTest"
-
-testCabalContents = unlines
-    ["name: "++testProjectName
-    ,"version:0.1"
-    ,"cabal-version:  >= 1.8"
-    ,"build-type:     Simple"
-    ,""
-    ,"library"
-    ,"  hs-source-dirs:  src"
-    ,"  exposed-modules: A"
-    ,"  other-modules:  B.C"
-    ,"  build-depends:  base"
-    ,""
-    ,"executable BWTest"
-    ,"  hs-source-dirs:  src"
-    ,"  main-is:         Main.hs"
-    ,"  other-modules:  B.D"
-    ,"  build-depends:  base"
-    ,"  ghc-options: -dynamic"
-    ,""
-    ,"test-suite BWTest-test"
-    ,"  type:            exitcode-stdio-1.0"
-    ,"  hs-source-dirs:  test"
-    ,"  main-is:         Main.hs"
-    ,"  other-modules:  TestA"
-    ,"  build-depends:  base"
+testFiles :: [(FilePath, [String])]
+testFiles =
+    [(projectName <.> "cabal",
+        ["name: " ++ projectName
+        ,"version:0.1"
+        ,"cabal-version:  >= 1.8"
+        ,"build-type:     Simple"
+        ,""
+        ,"library"
+        ,"  hs-source-dirs:  src"
+        ,"  exposed-modules: A"
+        ,"  other-modules:  B.C"
+        ,"  build-depends:  base"
+        ,""
+        ,"executable BWTest"
+        ,"  hs-source-dirs:  src"
+        ,"  main-is:         Main.hs"
+        ,"  other-modules:  B.D"
+        ,"  build-depends:  base"
+        ,"  ghc-options: -dynamic"
+        ,""
+        ,"test-suite BWTest-test"
+        ,"  type:            exitcode-stdio-1.0"
+        ,"  hs-source-dirs:  test"
+        ,"  main-is:         Main.hs"
+        ,"  other-modules:  TestA"
+        ,"  build-depends:  base"])
+    ,("Setup.hs",
+        ["#!/usr/bin/env runhaskell"
+        ,"import Distribution.Simple"
+        ,"main :: IO ()"
+        ,"main = defaultMain"])
+    ,("src/A.hs",
+        ["module A where"
+        ,"fA = undefined"])
+    ,("src/B/C.hs",
+        ["module B.C where"
+        ,"fC = undefined"])
+    ,("src/B/D.hs",
+        ["module B.D where"
+        ,"fD = undefined"])
+    ,("src/Main.hs",
+        ["module Main where"
+        ,"main = undefined"])
+    ,("test/Main.hs",
+        ["module Main where"
+        ,"main = undefined"])
+    ,("test/TestA.hs",
+        ["module TestA where"
+        ,"fTA = undefined"])
     ]
 
-testCabalFile :: FilePath -> FilePath
-testCabalFile root = root </> takeDirectory root <.> ".cabal"
+projectName = "BWTest"
 
-testAContents = unlines ["module A where","fA=undefined"]
-testCContents = unlines ["module B.C where","fC=undefined"]
-testDContents = unlines ["module B.D where","fD=undefined"]
-testMainContents = unlines ["module Main where","main=undefined"]
-testMainTestContents = unlines ["module Main where","main=undefined"]
-testTestAContents = unlines ["module TestA where","fTA=undefined"]
-
-testSetupContents = unlines
-    ["#!/usr/bin/env runhaskell"
-    ,"import Distribution.Simple"
-    ,"main :: IO ()"
-    ,"main = defaultMain"
-    ]
-
-createTestProject :: IO FilePath
-createTestProject = do
-    temp<-getTemporaryDirectory
-    let root=temp </> testProjectName
-    ex<-doesDirectoryExist root
-    when ex (removeDirectoryRecursive root)
-    createDirectory root
-    writeFile (testCabalFile root) testCabalContents
-    writeFile (root </> "Setup.hs") testSetupContents
-    let srcF=root </> "src"
-    createDirectory srcF
-    writeFile (srcF </> "A.hs") testAContents
-    let b=srcF </> "B"
-    createDirectory b
-    writeFile (b </> "C.hs") testCContents
-    writeFile (b </> "D.hs") testDContents
-    writeFile (srcF </> "Main.hs") testMainContents
-    let testF=root </> "test"
-    createDirectory testF
-    writeFile (testF </> "Main.hs") testMainTestContents
-    writeFile (testF </> "TestA.hs") testTestAContents
-    return root
+withTestProject :: (FilePath -> IO a) -> IO a
+withTestProject act = withTempDir $ \dir -> do
+    forM_ testFiles $ \(name,contents) -> do
+        createDirectoryIfMissing True $ takeDirectory $ dir </> name
+        writeFile (dir </> name) $ unlines contents
+    act dir
