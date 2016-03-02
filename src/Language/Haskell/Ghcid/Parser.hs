54f5a48c900499526c26f141d6232cc5ba93d801
Neil Mitchell <ndmitchell@gmail.com>
2016-03-02 16:47:14 +0000

    Fix parseLoad, was O(n * n * log n) by accident

--- src/Language/Haskell/Ghcid/Parser.hs
+++ src/Language/Haskell/Ghcid/Parser.hs
@@ -23,25 +23,24 @@ parseShowModules xs =
 -- | Parse messages given on reload.
 parseLoad :: [String] -> [Load]
 -- nub, because cabal repl sometimes does two reloads at the start
-parseLoad  = nubOrd . parseLoad'
-
--- | Parse messages given on reload
-parseLoad' :: [String] -> [Load]
-parseLoad' (('[':xs):rest) =
-    map (uncurry Loading) (parseShowModules [drop 11 $ dropWhile (/= ']') xs]) ++
-    parseLoad rest
-parseLoad' (x:xs)
-    | not $ " " `isPrefixOf` x
-    , (file,':':rest) <- break (== ':') x
-    , takeExtension file `elem` [".hs",".lhs"]
-    , (pos,rest2) <- span (\c -> c == ':' || isDigit c) rest
-    , [p1,p2] <- map read $ words $ map (\c -> if c == ':' then ' ' else c) pos
-    , (msg,las) <- span (isPrefixOf " ") xs
-    , rest3 <- trimStart rest2
-    , sev <- if "warning:" `isPrefixOf` lower rest3 then Warning else Error
-    = Message sev file (p1,p2) (x:msg) : parseLoad las
-parseLoad' (x:xs)
-    | Just file <- stripPrefix "<no location info>: can't find file: " x
-    = Message Error file (0,0) [file ++ ": Can't find file"] : parseLoad xs
-parseLoad' (_:xs) = parseLoad xs
-parseLoad' [] = []
+parseLoad  = nubOrd . f
+    where
+        f :: [String] -> [Load]
+        f (('[':xs):rest) =
+            map (uncurry Loading) (parseShowModules [drop 11 $ dropWhile (/= ']') xs]) ++
+            f rest
+        f (x:xs)
+            | not $ " " `isPrefixOf` x
+            , (file,':':rest) <- break (== ':') x
+            , takeExtension file `elem` [".hs",".lhs"]
+            , (pos,rest2) <- span (\c -> c == ':' || isDigit c) rest
+            , [p1,p2] <- map read $ words $ map (\c -> if c == ':' then ' ' else c) pos
+            , (msg,las) <- span (isPrefixOf " ") xs
+            , rest3 <- trimStart rest2
+            , sev <- if "warning:" `isPrefixOf` lower rest3 then Warning else Error
+            = Message sev file (p1,p2) (x:msg) : f las
+        f (x:xs)
+            | Just file <- stripPrefix "<no location info>: can't find file: " x
+            = Message Error file (0,0) [file ++ ": Can't find file"] : f xs
+        f (_:xs) = f xs
+        f [] = []
