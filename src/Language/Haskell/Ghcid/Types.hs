1d092aabc5b82fddc54f68f32a0beb0a1d12e509
Neil Mitchell <ndmitchell@gmail.com>
2016-03-02 08:49:05 +0000

    Tweak indentiation in the Types module

--- src/Language/Haskell/Ghcid/Types.hs
+++ src/Language/Haskell/Ghcid/Types.hs
@@ -14,25 +14,28 @@ newtype Ghci = Ghci (String -> IO [String])
 
 -- | GHCi shut down
 data GhciError = UnexpectedExit String String
-  deriving (Show,Eq,Ord,Typeable)
+    deriving (Show,Eq,Ord,Typeable)
 
 -- | Make GhciError an exception
 instance Exception GhciError
 
 -- | Severity of messages
 data Severity = Warning | Error
-  deriving (Show,Eq,Ord,Bounded,Enum,Typeable)
+    deriving (Show,Eq,Ord,Bounded,Enum,Typeable)
 
 -- | Load messages
 data Load
-    = Loading {loadModule :: String, loadFile :: FilePath}
+    = Loading
+        {loadModule :: String
+        ,loadFile :: FilePath
+        }
     | Message
         {loadSeverity :: Severity
         ,loadFile :: FilePath
         ,loadFilePos :: (Int,Int)
         ,loadMessage :: [String]
         }
-      deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord)
 
 -- | Is a Load a message with severity?
 isMessage :: Load -> Bool
