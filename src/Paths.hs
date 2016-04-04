d67615dad2a76e04a5eea19ac1350c43202acf61
Neil Mitchell <ndmitchell@gmail.com>
2016-04-04 07:44:44 +0100

    Use makeVersion in Paths

--- src/Paths.hs
+++ src/Paths.hs
@@ -1,10 +1,7 @@
 
 module Paths_ghcid where
 
-import Data.Version
-import Text.ParserCombinators.ReadP
-
+import Data.Version.Extra
 
 version :: Version
--- can't write a literal Version value since in GHC 7.10 the versionsTag field is deprecated
-version = head $ [v | (v,"") <- readP_to_S parseVersion "0.0"] ++ error "version, failed to parse"
+version = makeVersion [0,0]
