d2d2a17cfccdf166c2136ef14af12436324ad622
Neil Mitchell <ndmitchell@gmail.com>
2016-07-18 22:35:39 +0100

    Rewrite the parser tests

--- src/Test/Parser.hs
+++ src/Test/Parser.hs
@@ -10,30 +10,21 @@ import Language.Haskell.Ghcid.Types
 
 parserTests :: TestTree
 parserTests = testGroup "Parser tests"
-    [testShowModules
+    [testParseShowModules
     ,testParseLoad
     ]
 
-testShowModules :: TestTree
-testShowModules = testCase "Show Modules" $ parseShowModules
+testParseShowModules :: TestTree
+testParseShowModules = testCase "Show Modules" $ parseShowModules
     ["Main             ( src/Main.hs, interpreted )"
     ,"AI.Neural.WiscDigit ( src/AI/Neural/WiscDigit.hs, interpreted )"
-    ] @?= [("Main","src/Main.hs"),("AI.Neural.WiscDigit","src/AI/Neural/WiscDigit.hs")]
-
-
-testParseLoad :: TestTree
-testParseLoad = testGroup "Load Parsing"
-    [testCase "output1" $ parseLoad output1 @?=
-        [Loading "GHCi" "GHCi.hs"
-        ,Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (70,1), loadMessage = ["GHCi.hs:70:1: Parse error: naked expression at top level"]}
-        ,Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (72,13), loadMessage = ["GHCi.hs:72:13:","    No instance for (Num ([String] -> [String]))","      arising from the literal `1'","    Possible fix:","      add an instance declaration for (Num ([String] -> [String]))","    In the expression: 1","    In an equation for `parseLoad': parseLoad = 1"]}
-        ,Message {loadSeverity = Warning, loadFile = "GHCi.hs", loadFilePos = (81,1), loadMessage = ["GHCi.hs:81:1: Warning: Defined but not used: `foo'"]}
-        ,Message {loadSeverity = Warning, loadFile = "C:\\GHCi.hs", loadFilePos = (82,1), loadMessage = ["C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217"]}
-        ]
+    ] @?=
+    [("Main","src/Main.hs")
+    ,("AI.Neural.WiscDigit","src/AI/Neural/WiscDigit.hs")
     ]
 
-output1 :: [String]
-output1 =
+testParseLoad :: TestTree
+testParseLoad = testCase "Load Parsing" $ parseLoad
     ["[1 of 2] Compiling GHCi             ( GHCi.hs, interpreted )"
     ,"GHCi.hs:70:1: Parse error: naked expression at top level"
     ,"GHCi.hs:72:13:"
@@ -45,4 +36,10 @@ output1 =
     ,"    In an equation for `parseLoad': parseLoad = 1"
     ,"GHCi.hs:81:1: Warning: Defined but not used: `foo'"
     ,"C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217" -- GHC 7.12 uses lowercase
+    ] @?=
+    [Loading "GHCi" "GHCi.hs"
+    ,Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (70,1), loadMessage = ["GHCi.hs:70:1: Parse error: naked expression at top level"]}
+    ,Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (72,13), loadMessage = ["GHCi.hs:72:13:","    No instance for (Num ([String] -> [String]))","      arising from the literal `1'","    Possible fix:","      add an instance declaration for (Num ([String] -> [String]))","    In the expression: 1","    In an equation for `parseLoad': parseLoad = 1"]}
+    ,Message {loadSeverity = Warning, loadFile = "GHCi.hs", loadFilePos = (81,1), loadMessage = ["GHCi.hs:81:1: Warning: Defined but not used: `foo'"]}
+    ,Message {loadSeverity = Warning, loadFile = "C:\\GHCi.hs", loadFilePos = (82,1), loadMessage = ["C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217"]}
     ]
