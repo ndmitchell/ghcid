9520c6067f5f9e3c437989259413fbd4fecf4ba2
Neil Mitchell <ndmitchell@gmail.com>
2016-07-18 22:13:35 +0100

    Reformat some tests

--- src/Test/Parser.hs
+++ src/Test/Parser.hs
@@ -1,7 +1,5 @@
 -- | Test the message parser
-module Test.Parser
-  ( parserTests
-  )where
+module Test.Parser(parserTests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -11,40 +9,40 @@ import Language.Haskell.Ghcid.Types
 
 
 parserTests :: TestTree
-parserTests=testGroup "Parser tests"
-  [ testShowModules
-  , testParseLoad
-  ]
+parserTests = testGroup "Parser tests"
+    [testShowModules
+    ,testParseLoad
+    ]
 
 testShowModules :: TestTree
-testShowModules =  testCase "Show Modules" $ parseShowModules [
-    "Main             ( src/Main.hs, interpreted )",
-    "AI.Neural.WiscDigit ( src/AI/Neural/WiscDigit.hs, interpreted )"
+testShowModules = testCase "Show Modules" $ parseShowModules
+    ["Main             ( src/Main.hs, interpreted )"
+    ,"AI.Neural.WiscDigit ( src/AI/Neural/WiscDigit.hs, interpreted )"
     ] @?= [("Main","src/Main.hs"),("AI.Neural.WiscDigit","src/AI/Neural/WiscDigit.hs")]
 
 
 testParseLoad :: TestTree
 testParseLoad = testGroup "Load Parsing"
-  [ testCase "output1" $ parseLoad output1 @?=
-    [ Loading "GHCi" "GHCi.hs"
-    , Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (70,1), loadMessage = ["GHCi.hs:70:1: Parse error: naked expression at top level"]}
-    , Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (72,13), loadMessage = ["GHCi.hs:72:13:","    No instance for (Num ([String] -> [String]))","      arising from the literal `1'","    Possible fix:","      add an instance declaration for (Num ([String] -> [String]))","    In the expression: 1","    In an equation for `parseLoad': parseLoad = 1"]}
-    , Message {loadSeverity = Warning, loadFile = "GHCi.hs", loadFilePos = (81,1), loadMessage = ["GHCi.hs:81:1: Warning: Defined but not used: `foo'"]}
-    , Message {loadSeverity = Warning, loadFile = "C:\\GHCi.hs", loadFilePos = (82,1), loadMessage = ["C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217"]}
+    [testCase "output1" $ parseLoad output1 @?=
+        [Loading "GHCi" "GHCi.hs"
+        ,Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (70,1), loadMessage = ["GHCi.hs:70:1: Parse error: naked expression at top level"]}
+        ,Message {loadSeverity = Error, loadFile = "GHCi.hs", loadFilePos = (72,13), loadMessage = ["GHCi.hs:72:13:","    No instance for (Num ([String] -> [String]))","      arising from the literal `1'","    Possible fix:","      add an instance declaration for (Num ([String] -> [String]))","    In the expression: 1","    In an equation for `parseLoad': parseLoad = 1"]}
+        ,Message {loadSeverity = Warning, loadFile = "GHCi.hs", loadFilePos = (81,1), loadMessage = ["GHCi.hs:81:1: Warning: Defined but not used: `foo'"]}
+        ,Message {loadSeverity = Warning, loadFile = "C:\\GHCi.hs", loadFilePos = (82,1), loadMessage = ["C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217"]}
+        ]
     ]
-  ]
 
 output1 :: [String]
-output1=
-  [ "[1 of 2] Compiling GHCi             ( GHCi.hs, interpreted )"
-  , "GHCi.hs:70:1: Parse error: naked expression at top level"
-  , "GHCi.hs:72:13:"
-  , "    No instance for (Num ([String] -> [String]))"
-  , "      arising from the literal `1'"
-  , "    Possible fix:"
-  , "      add an instance declaration for (Num ([String] -> [String]))"
-  , "    In the expression: 1"
-  , "    In an equation for `parseLoad': parseLoad = 1"
-  , "GHCi.hs:81:1: Warning: Defined but not used: `foo'"
-  , "C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217" -- GHC 7.12 uses lowercase
-  ]
+output1 =
+    ["[1 of 2] Compiling GHCi             ( GHCi.hs, interpreted )"
+    ,"GHCi.hs:70:1: Parse error: naked expression at top level"
+    ,"GHCi.hs:72:13:"
+    ,"    No instance for (Num ([String] -> [String]))"
+    ,"      arising from the literal `1'"
+    ,"    Possible fix:"
+    ,"      add an instance declaration for (Num ([String] -> [String]))"
+    ,"    In the expression: 1"
+    ,"    In an equation for `parseLoad': parseLoad = 1"
+    ,"GHCi.hs:81:1: Warning: Defined but not used: `foo'"
+    ,"C:\\GHCi.hs:82:1: warning: Defined but not used: \8216foo\8217" -- GHC 7.12 uses lowercase
+    ]
--- src/Test/Util.hs
+++ src/Test/Util.hs
@@ -1,7 +1,5 @@
 -- | Test utility functions
-module Test.Util
-  ( utilsTests
-  ) where
+module Test.Util(utilsTests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -9,21 +7,21 @@ import Test.Tasty.HUnit
 import Language.Haskell.Ghcid.Util
 
 utilsTests :: TestTree
-utilsTests=testGroup "Utility tests"
-  [ dropPrefixTests
-  , chunksOfWordTests
-  ]
+utilsTests = testGroup "Utility tests"
+    [dropPrefixTests
+    ,chunksOfWordTests
+    ]
 
 dropPrefixTests :: TestTree
 dropPrefixTests = testGroup "dropPrefix"
-  [ testCase "Prefix not found" $ dropPrefixRepeatedly "prefix" "string"  @?= "string"
-  , testCase "Empty prefix" $ dropPrefixRepeatedly "" "string" @?= "string"
-  , testCase "Prefix found once" $ dropPrefixRepeatedly "str" "string" @?= "ing"
-  , testCase "Prefix found twice" $ dropPrefixRepeatedly "str" "strstring" @?= "ing"
-  ]
+    [testCase "Prefix not found" $ dropPrefixRepeatedly "prefix" "string"  @?= "string"
+    ,testCase "Empty prefix" $ dropPrefixRepeatedly "" "string" @?= "string"
+    ,testCase "Prefix found once" $ dropPrefixRepeatedly "str" "string" @?= "ing"
+    ,testCase "Prefix found twice" $ dropPrefixRepeatedly "str" "strstring" @?= "ing"
+    ]
 
 chunksOfWordTests :: TestTree
 chunksOfWordTests = testGroup "chunksOfWord"
-  [ testCase "Max 0" $ chunksOfWord 4 0 "ab cd efgh" @?= ["ab c","d ef","gh"]
-  , testCase "Max 2" $ chunksOfWord 4 2 "ab cd efgh" @?= ["ab ","cd ","efgh"]
-  ]
+    [testCase "Max 0" $ chunksOfWord 4 0 "ab cd efgh" @?= ["ab c","d ef","gh"]
+    ,testCase "Max 2" $ chunksOfWord 4 2 "ab cd efgh" @?= ["ab ","cd ","efgh"]
+    ]
