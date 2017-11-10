5ea1abdafd01387686f2319d3677dee1576c5670
Neil Mitchell <ndmitchell@gmail.com>
2017-11-10 20:00:38 +0000

    [vscode] Rename watchGhcid to watchGhcidOutput, to match its label

--- plugins/vscode/src/extension.ts
+++ plugins/vscode/src/extension.ts
@@ -92,7 +92,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.watchGhcid', () => {
+    let disposable = vscode.commands.registerCommand('extension.watchGhcidOutput', () => {
         try {
             let d = vscode.languages.createDiagnosticCollection('ghcid');
             let file = vscode.window.activeTextEditor.document.uri.fsPath;
