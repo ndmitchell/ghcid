0a9bc67c6aa08d51cdd6cb8f19738c359fd1757f
Neil Mitchell <ndmitchell@gmail.com>
2018-04-08 21:38:10 +0100

    HLint fixes

--- src/Ghcid.hs
+++ src/Ghcid.hs
@@ -212,7 +212,7 @@ runGhcid session waiter termSize termOutput opts@Options{..} = do
             let n = height - length msg
             currTime <- getShortTime
             load <- return $ take (if isJust load then n else 0) $ prettyOutput max_messages currTime (maybe 0 fst load)
-                [ m{loadMessage = map fromEsc $ concatMap (chunksOfWordE width (width `div` 5)) $ map Esc $ loadMessage m}
+                [ m{loadMessage = map fromEsc $ concatMap (chunksOfWordE width (width `div` 5) . Esc) $ loadMessage m}
                 | m@Message{} <- maybe [] snd load]
             termOutput $ load ++ map (Plain,) msg ++ replicate (height - (length load + length msg)) (Plain,"")
 
