7932949ef8fb5792f28581859aca53a4c793214e
Neil Mitchell <ndmitchell@gmail.com>
2018-04-08 11:07:23 +0100

    Make all the message parsing escape-code aware

--- src/Language/Haskell/Ghcid/Parser.hs
+++ src/Language/Haskell/Ghcid/Parser.hs
@@ -7,7 +7,9 @@ module Language.Haskell.Ghcid.Parser(
 
 import System.FilePath
 import Data.Char
+import Data.Either.Extra
 import Data.List.Extra
+import Data.Maybe
 import Data.Tuple.Extra
 import Control.Applicative
 import Prelude
@@ -15,13 +17,18 @@ import Prelude
 import Language.Haskell.Ghcid.Types
 
 -- A string with escape characters in it
-newtype Esc = Esc String
+newtype Esc = Esc {fromEsc :: String}
+
+app (Esc x) (Esc y) = Esc $ x ++ y
+
+unesc :: Esc -> Maybe (Either Esc Char, Esc)
+unesc (Esc ('\ESC':xs)) | (pre,'m':post) <- break (== 'm') xs = Just (Left $ Esc $ '\ESC':pre++"m", Esc post)
+unesc (Esc (x:xs)) = Just (Right x, Esc xs)
+unesc (Esc []) = Nothing
 
 -- | Remove all escape characters in a string
 unescape :: Esc -> String
-unescape (Esc ('\ESC':xs)) | (pre,'m':post) <- break (== 'm') xs = unescape (Esc post)
-unescape (Esc (x:xs)) = x : unescape (Esc xs)
-unescape (Esc []) = []
+unescape = rights . unfoldr unesc
 
 
 -- | Parse messages from show modules command. Given the parsed lines
@@ -32,46 +39,80 @@ parseShowModules (map Esc -> xs) =
     [ (takeWhile (not . isSpace) $ trimStart a, takeWhile (/= ',') b)
     | x <- map unescape xs, (a,'(':' ':b) <- [break (== '(') x]]
 
+stripPrefixE :: String -> Esc -> Maybe Esc
+stripPrefixE [] e = Just e
+stripPrefixE (x:xs) e = case unesc e of
+    Just (Left code, rest) -> fmap (app code) $ stripPrefixE (x:xs) rest
+    Just (Right y, rest) | y == x -> stripPrefixE xs rest
+    _ -> Nothing
+
+stripInfixE :: String -> Esc -> Maybe (Esc, Esc)
+stripInfixE needle haystack | Just rest <- stripPrefixE needle haystack = Just (Esc [], rest)
+stripInfixE needle e = case unesc e of
+    Nothing -> Nothing
+    Just (x,xs) -> first (app $ fromEither $ fmap (Esc . return) x) <$> stripInfixE needle xs
+
+
+spanE :: (Char -> Bool) -> Esc -> (Esc, Esc)
+spanE f e = case unesc e of
+    Nothing -> (Esc "", Esc "")
+    Just (Left e, rest) -> first (app e) $ spanE f rest
+    Just (Right c, rest) | f c -> first (app $ Esc [c]) $ spanE f rest
+                         | otherwise -> (Esc "", e)
+
+isPrefixOfE :: String -> Esc -> Bool
+isPrefixOfE x y = isJust $ stripPrefixE x y
+
+trimStartE :: Esc -> Esc
+trimStartE e = case unesc e of
+    Nothing -> Esc ""
+    Just (Left code, rest) -> app code $ trimStartE rest
+    Just (Right c, rest) | isSpace c -> trimStartE rest
+                         | otherwise -> e
+
+
+unwordsE :: [Esc] -> Esc
+unwordsE = Esc . unwords . map fromEsc
 
 -- | Parse messages given on reload.
 parseLoad :: [String] -> [Load]
 -- nub, because cabal repl sometimes does two reloads at the start
-parseLoad (map Esc -> xs) = nubOrd $ f $ map unescape xs
+parseLoad (map Esc -> xs) = nubOrd $ f xs
     where
-        f :: [String] -> [Load]
+        f :: [Esc] -> [Load]
 
         -- [1 of 2] Compiling GHCi             ( GHCi.hs, interpreted )
         f (xs:rest)
-            | Just xs <- stripPrefix "[" xs
-            = map (uncurry Loading) (parseShowModules [drop 11 $ dropWhile (/= ']') xs]) ++
+            | Just xs <- stripPrefixE "[" xs
+            = map (uncurry Loading) (parseShowModules [drop 11 $ dropWhile (/= ']') $ unescape xs]) ++
               f rest
 
         -- GHCi.hs:81:1: Warning: Defined but not used: `foo'
         f (x:xs)
-            | not $ " " `isPrefixOf` x
+            | not $ " " `isPrefixOfE` x
             , Just (file,rest) <- breakFileColon x
             , takeExtension file `elem` [".hs",".lhs",".hs-boot",".lhs-boot"]
              -- take position, including span if present
-            , (pos,rest) <- span (\c -> c == ':' || c == '-' || isSpan c || isDigit c) rest
+            , (pos,rest) <- spanE (\c -> c == ':' || c == '-' || isSpan c || isDigit c) rest
             -- separate line and column, ignoring span (we want the start point only)
-            , [p1,p2] <- map read $ wordsBy (\c -> c == ':' || isSpan c) $ takeWhile (/= '-') pos
+            , [p1,p2] <- map read $ wordsBy (\c -> c == ':' || isSpan c) $ takeWhile (/= '-') $ unescape pos
             , (msg,las) <- span isMessageBody xs
-            , rest <- trimStart $ unwords $ rest : xs
-            , sev <- if "warning:" `isPrefixOf` lower rest then Warning else Error
-            = Message sev file (p1,p2) (x:msg) : f las
+            , rest <- trimStartE $ unwordsE $ rest : xs
+            , sev <- if "warning:" `isPrefixOf` lower (unescape rest) then Warning else Error
+            = Message sev file (p1,p2) (map unescape $ x:msg) : f las
 
         -- <no location info>: can't find file: FILENAME
         f (x:xs)
-            | Just file <- stripPrefix "<no location info>: can't find file: " x
-            = Message Error file (0,0) [file ++ ": Can't find file"] : f xs
+            | Just file <- stripPrefixE "<no location info>: can't find file: " x
+            = Message Error (unescape file) (0,0) [unescape file ++ ": Can't find file"] : f xs
 
         -- Module imports form a cycle:
         --   module `Module' (Module.hs) imports itself
         f (x:xs)
-            | x == "Module imports form a cycle:"
-            , (xs,rest) <- span (isPrefixOf " ") xs
-            , let ms = [takeWhile (/= ')') x | x <- xs, '(':x <- [dropWhile (/= '(') x]]
-            = Message Error "" (0,0) (x:xs) :
+            | unescape x == "Module imports form a cycle:"
+            , (xs,rest) <- span (isPrefixOfE " ") xs
+            , let ms = [takeWhile (/= ')') x | x <- xs, '(':x <- [dropWhile (/= '(') $ unescape x]]
+            = Message Error "" (0,0) (map unescape $ x:xs) :
               -- need to label the modules in the import cycle so I can find them
               [Message Error m (0,0) [] | m <- nubOrd ms] ++ f rest
         f (_:xs) = f xs
@@ -79,12 +120,15 @@ parseLoad (map Esc -> xs) = nubOrd $ f $ map unescape xs
         isSpan c = c== ',' || c == '(' || c == ')'
 
 -- After the file location, message bodies are indented (perhaps prefixed by a line number)
-isMessageBody :: String -> Bool
-isMessageBody xs = isPrefixOf " " xs || case break (== '|') xs of
-  (prefix, _:_) | all (\x -> isSpace x || isDigit x) prefix -> True
+isMessageBody :: Esc -> Bool
+isMessageBody xs = isPrefixOfE " " xs || case stripInfixE "|" xs of
+  Just (prefix, _) | all (\x -> isSpace x || isDigit x) $ unescape prefix -> True
   _ -> False
 
 -- A filename, followed by a colon - be careful to handle Windows drive letters, see #61
-breakFileColon :: String -> Maybe (FilePath, String)
-breakFileColon (x:':':xs) | isLetter x = first ([x,':']++) <$> stripInfix ":" xs
-breakFileColon xs = stripInfix ":" xs
+breakFileColon :: Esc -> Maybe (FilePath, Esc)
+breakFileColon xs = case stripInfixE ":" xs of
+    Nothing -> Nothing
+    Just (a,b)
+        | [drive] <- unescape a, isLetter drive -> first ((++) [drive,':'] . unescape) <$> stripInfixE ":" b
+        | otherwise -> Just (unescape a, b)
