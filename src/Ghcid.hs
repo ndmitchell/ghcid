fdc2190670a7774f451bb3b18b7c0da8d6f4d675
Lennart Spitzner <hexagoxel@hexagoxel.de>
2020-06-15 22:49:43 +0200

    Print final message to file, Parse options once only

--- src/Ghcid.hs
+++ src/Ghcid.hs
@@ -201,10 +201,27 @@ handleErrors = handle $ \(UnexpectedExit cmd _ mmsg) -> do
         Nothing -> ""
     exitFailure
 
+finallyPrintStopped :: Options -> IO () -> IO ()
+finallyPrintStopped opts x = x `finally` do
+        outputfile opts `forM_` \file -> do
+            writeFile file "ghcid has stopped\n"
+
+
 -- | Like 'main', but run with a fake terminal for testing
 mainWithTerminal :: IO TermSize -> ([String] -> IO ()) -> IO ()
-mainWithTerminal termSize termOutput =
-    handleErrors $
+mainWithTerminal termSize termOutput = do
+    opts <- withGhcidArgs $ cmdArgsRun options
+    whenLoud $ do
+        outStrLn $ "%OS: " ++ os
+        outStrLn $ "%ARCH: " ++ arch
+        outStrLn $ "%VERSION: " ++ showVersion version
+        args <- getArgs
+        outStrLn $ "%ARGUMENTS: " ++ show args
+    mainWithTerminalWithOpts termSize termOutput opts
+
+mainWithTerminalWithOpts :: IO TermSize -> ([String] -> IO ()) -> Options -> IO ()
+mainWithTerminalWithOpts termSize termOutput opts = do
+    finallyPrintStopped opts $ handleErrors $
         forever $ withWindowIcon $ withSession $ \session -> do
             setVerbosity Normal -- undo any --verbose flags
 
@@ -212,13 +229,6 @@ mainWithTerminal termSize termOutput =
             hSetBuffering stdout LineBuffering
             hSetBuffering stderr NoBuffering
             origDir <- getCurrentDirectory
-            opts <- withGhcidArgs $ cmdArgsRun options
-            whenLoud $ do
-                outStrLn $ "%OS: " ++ os
-                outStrLn $ "%ARCH: " ++ arch
-                outStrLn $ "%VERSION: " ++ showVersion version
-                args <- getArgs
-                outStrLn $ "%ARGUMENTS: " ++ show args
             withCurrentDirectory (directory opts) $ do
                 opts <- autoOptions opts
                 opts <- pure $ opts{restart = nubOrd $ (origDir </> ".ghcid") : restart opts, reload = nubOrd $ reload opts}
