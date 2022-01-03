d0384c511b533df0cf3e1f982f0afd1d9a65f489
Neil Mitchell <ndmitchell@gmail.com>
2022-01-03 17:19:13 +0000

    #348, adjust the language around ghci with no files

--- src/Ghcid.hs
+++ src/Ghcid.hs
@@ -326,7 +326,7 @@ runGhcid session waiter termSize termOutput opts@Options{..} = do
         map (":set " ++) (ghciFlagsUseful ++ ghciFlagsUsefulVersioned) ++ setup
 
     when (null loaded && not ignoreLoaded) $ do
-        putStrLn $ "\nNo files loaded, GHCi is not working properly.\nCommand: " ++ command
+        putStrLn $ "\nNo files loaded, GHCi not configured properly.\nCommand: " ++ command
         exitFailure
 
     restart <- pure $ nubOrd $ restart ++ [x | LoadConfig x <- messages]
