a649a7e0c5040e0444587792b50d976bcd0efafc
Neil Mitchell <ndmitchell@gmail.com>
2022-01-04 12:03:51 +0000

    Adjust the test

--- src/Test/API.hs
+++ src/Test/API.hs
@@ -11,7 +11,7 @@ import Language.Haskell.Ghcid.Util
 
 
 apiTests :: TestTree
-apiTests = testGroup "API test" $ take 1
+apiTests = testGroup "API test" $ drop 1 -- TEMPORARY
     [testCase "No files" $ withTempDir $ \dir -> do
         (ghci,load) <- startGhci "ghci -ignore-dot-ghci" (Just dir) $ const putStrLn
         load @?= []
