0dd6b74af5da4418d9ddcfec36c2d07405b0a6b2
Chris Wendt <chrismwendt@gmail.com>
2026-02-21 20:15:43 -0500

    yarn upgrade --latest

--- plugins/vscode/test/index.ts
+++ plugins/vscode/test/index.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -10,28 +10,23 @@ export function run(): Promise<void> {
   });
 
   const testsRoot = path.resolve(__dirname, '..');
+  const files = await glob('**/**.test.js', { cwd: testsRoot });
 
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
+  // Add files to the test suite
+  files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-      // Add files to the test suite
-      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run(failures => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        e(err);
-      }
-    });
+  return new Promise<void>((c, e) => {
+    try {
+      // Run the mocha test
+      mocha.run(failures => {
+        if (failures > 0) {
+          e(new Error(`${failures} tests failed.`));
+        } else {
+          c();
+        }
+      });
+    } catch (err) {
+      e(err);
+    }
   });
 }
