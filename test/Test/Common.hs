f2be00a9a0c293e877d72e500658847cc35468c7
Chris Wendt <chrismwendt@gmail.com>
2026-03-13 18:58:34 -0400

    .

--- test/Test/Common.hs
+++ test/Test/Common.hs
@@ -9,5 +9,5 @@ import Data.Version
 -- readily resetting which packages are loaded
 disable19650 :: TestTree -> TestTree
 disable19650 x
-    | compilerVersion < makeVersion [9] = x
-    | otherwise = testGroup "Disabled" []
+    | compilerVersion == makeVersion [9,0] = testGroup "Disabled" []
+    | otherwise = x
